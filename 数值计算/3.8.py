ccc9d2d5f25d2339a6c9cd8eee9c8ca8de096e7d
jijunhao <2818657803@qq.com>
2020-07-22 15:00:31 +0800

    第一次修改

--- 数值计算/3.8.py
+++ 数值计算/3.8.py
@@ -10,6 +10,5 @@ y = np.array([0.88,1.22,1.64,2.72,3.96,7.66,11.96,21.56,43.16])
 b = [[sum(y*x_0.T)],
      [sum(y*x_1.T)],
      [sum(y*x_2.T)]]
-print(b)
 a = np.linalg.solve(A,np.array(b))
 print(a)
